b63cfa7f4f434b594fe3bda4eccfa05e6da29220
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-03-13 14:54:05 -0300

    ssasasa

--- comparar-escolas.js
+++ comparar-escolas.js
@@ -30,15 +30,17 @@ class Senai {
 
 
 /* ===== OBTER ELEMENTOS ===== */
-let codigoA = document.querySelectorAll('.unidade__input unidadeA__codigo');
-let cidadeA = document.querySelectorAll('.unidade__input unidadeA__cidade');
-let anoConstrucaoA = document.querySelectorAll('.unidade__input unidadeA__ano');
-let qtdeCursosA = document.querySelectorAll('.unidade__input unidadeA__cursos');
+let codigoA = document.querySelector('.unidadeA__codigo');
+let cidadeA = document.querySelector('.unidadeA__cidade');
+let anoConstrucaoA = document.querySelector('.unidadeA__ano');
+let qtdeCursosA = document.querySelector('.unidadeA__cursos');
 
-let codigoB = document.querySelectorAll('.unidade__input unidadeB__codigo');
-let cidadeB = document.querySelectorAll('.unidade__input unidadeB__cidade');
-let anoConstrucaoB = document.querySelectorAll('.unidade__input unidadeB__ano');
-let qtdeCursosB = document.querySelectorAll('.unidade__input unidadeB__cursos');
+let codigoB = document.querySelector('.unidadeB__codigo');
+let cidadeB = document.querySelector('.unidadeB__cidade');
+let anoConstrucaoB = document.querySelector('.unidadeB__ano');
+let qtdeCursosB = document.querySelector('.unidadeB__cursos');
+
+
 
 /* ===== FUNÇÃO DE VALIDAÇÃO ===== */
 
@@ -112,19 +114,40 @@ function fecharEscolaB (){
 }
 
 /* ===== RELATÓRIO DE COMPARAÇÃO ===== */
-function compararEscolas (e1, e2){
-  console.log(`> Comparando oferta de cursos: ${e1.cidade} vs ${e2.cidade}...`);
+function compararEscolas (){
 
-  if (e1.qtdeCursos > e2.qtdeCursos) {
-      console.log(`Resultado: O SENAI ${e1.cidade} possui mais cursos.`);
-  } else if (e2.qtdeCursos > e1.qtdeCursos) {
-      console.log(`Resultado: O SENAI ${e2.cidade} possui mais cursos.`);
+  document.querySelector('.relatorio--oculto').style.display= 'flex'
+  if (escolaA.qtdeCursos > escolaB.qtdeCursos) {
+    document.querySelector('.relatorio__veredito').innerText = `Veredito ${escolaA.cidade} lidera com ${escolaA.qtdeCursos}`;
+  } else if (escolaB.qtdeCursos > escolaA.qtdeCursos) {
+    document.querySelector('.relatorio__veredito').innerText = `Veredito ${escolaB.cidade} lidera com ${escolaB.qtdeCursos}`;
   } else {
-      console.log(`Resultado: Ambas as unidades possuem a mesma quantidade de cursos.`);
+      document.querySelector('.relatorio__veredito').innerText = `Veredito: Ambas as unidades possuem a mesma quantidade de cursos.`;
   }
-  console.log(`Status: ${e1.cidade} (${e1.qtdeCursos}) | ${e2.cidade} (${e2.qtdeCursos})\n`);
+  document.querySelector('.relatorio__veredito').innerText = `Unidade A: código: ${escolaA.codigo} cidade: ${escolaA.cidade} ${escolaA.qtdeCursos}`;
 }
+  
+document.querySelector('.sistema__comparar').addEventListener('click', compararEscolas);
 
 /* ===== NOVA CONSULTA ===== */
+function nova_consulta() {
+  document.querySelector('.relatorio--oculto').style.display= 'none'
+
+  codigoA.value = '';
+  cidadeA.value = '';
+  anoConstrucaoA.value = '';
+  qtdeCursosA.value = '';
+
+  codigoB.value = '';
+  cidadeB.value = '';
+  anoConstrucaoB.value = '';
+  qtdeCursosB.value = '';
+
+  cidadeA.focus()
+}
+
+// botao nova consulta
+
+document.querySelector('.sistema__reset').addEventListener('click', nova_consulta);
 
 
